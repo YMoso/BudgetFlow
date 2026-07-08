b6c075640ed2bc1cb36693606c879b6bc4b09aab
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-07-09 00:08:53 +0200

    chore: prepare app for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+import.meta.env.VITE_API_URL = undefined;
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
 
   if (token) {
-      config.headers.Authorization = `Bearer ${token}`;}
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
   return config;
 });
